46955181f3cd570d64e2a6e01ae8912f3ea1d0a7
maciejlewandowski5 <maciejlewandowski5@github.com>
2020-05-05 22:06:04 +0200

    Added funtionality to button reserMousePosition in all activites. Added exported atribute set to true for every acivity in android manifest in order for easier page testing on phone.

--- app/src/main/java/com/example/controler/MusicPage.java
+++ app/src/main/java/com/example/controler/MusicPage.java
@@ -1,14 +1,28 @@
 package com.example.controler;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MusicPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+    public void resetMousePointer(View view) {
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("X");
+        myRef.setValue("0");
+        myRef = database.getReference("Y");
+        myRef.setValue("0");
+        myRef = database.getReference("Z");
+        myRef.setValue("0");
     }
 }
--- app/src/main/java/com/example/controler/WagglePage.java
+++ app/src/main/java/com/example/controler/WagglePage.java
@@ -3,6 +3,10 @@ package com.example.controler;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class WagglePage extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class WagglePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_waggle_page);
+    }
+    public void resetMousePointer(View view) {
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("X");
+        myRef.setValue("0");
+        myRef = database.getReference("Y");
+        myRef.setValue("0");
+        myRef = database.getReference("Z");
+        myRef.setValue("0");
     }
 }
