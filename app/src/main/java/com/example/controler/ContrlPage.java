515fe596604d36e5586bcf99b8a1995a883f3a4d
maciejlewandowski5 <maciejlewandowski5@github.com>
2020-05-08 11:10:02 +0200

    Controilig cursor on the app desktop app using phone now is working.

--- app/src/main/java/com/example/controler/ContrlPage.java
+++ app/src/main/java/com/example/controler/ContrlPage.java
@@ -4,21 +4,18 @@ package com.example.controler;
 import androidx.appcompat.app.AppCompatActivity;
 
 
-import android.annotation.SuppressLint;
 import android.content.Context;
-import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.icu.math.BigDecimal;
 import android.os.Bundle;
 
-import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
+
 import java.lang.Math;
+import java.util.ArrayList;
 
 
 import com.google.firebase.database.DatabaseReference;
@@ -26,6 +23,7 @@ import com.google.firebase.database.FirebaseDatabase;
 
 import processing.core.PApplet;
 
+import static java.lang.Math.asin;
 import static java.lang.Math.tan;
 
 
@@ -34,16 +32,27 @@ public class ContrlPage extends AppCompatActivity implements SensorEventListener
     private SensorManager sensorManager;
     private Sensor sensor;
     private double intialX;
+    private int doResetSensorIfThisSmallerThanTen = 0 ;
+    private ArrayList<Double> xi = new ArrayList<>();
+    private ArrayList<Double> yi = new ArrayList<>();
+    private ArrayList<Double> zi = new ArrayList<>();
+    private Double X;
+    private Double Y;
+    private Double Z;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_contrl_page);
+
+        X=0.0;
+        Y=0.0;
+        Z=0.0;
+
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         sensor = sensorManager.getDefaultSensor(Sensor.TYPE_GAME_ROTATION_VECTOR);
-        sensorManager.registerListener( ContrlPage.this,sensor,SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener( ContrlPage.this,sensor,SensorManager.SENSOR_DELAY_GAME);
 
     }
     @Override
@@ -53,27 +62,51 @@ public class ContrlPage extends AppCompatActivity implements SensorEventListener
     @Override
     public void onSensorChanged(SensorEvent sensorEvent){
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("X");
-
-        myRef.setValue(sensorEvent.values[0] );
-        myRef = database.getReference("Y");
-        myRef.setValue(sensorEvent.values[1] );
-        myRef = database.getReference("Z");
-        myRef.setValue(sensorEvent.values[2]);
+        if(doResetSensorIfThisSmallerThanTen >11) {
+            DatabaseReference myRef = database.getReference("X");
+            myRef.setValue(50 +  100 * 0.5 * tan(2 * asin(sensorEvent.values[2] - X)));
+            myRef = database.getReference("Y");
+            myRef.setValue(50 +  100 * 0.5 * tan(2 * asin(sensorEvent.values[0] - Y)));
+            myRef = database.getReference("Z");
+            myRef.setValue(50 +  100 * 0.5 * tan(2 * asin(sensorEvent.values[1] - Z) ));
+        } else if (doResetSensorIfThisSmallerThanTen <11){
+           xi.add((double) sensorEvent.values[2]);
+           yi.add((double) sensorEvent.values[0]);
+           zi.add((double) sensorEvent.values[1]);
+           doResetSensorIfThisSmallerThanTen = doResetSensorIfThisSmallerThanTen + 1;
+        }else{
+            X = getMean(xi);
+            Y = getMean(yi);
+            Z = getMean(zi);
+            xi.clear();
+            yi.clear();
+            zi.clear();
+            doResetSensorIfThisSmallerThanTen = doResetSensorIfThisSmallerThanTen + 1;
+        }
 
 
     }
 
+    private Double getMean(ArrayList<Double> a){
+        Double sum = 0.0;
+        for( int i=0 ; i < a.size() ; i++ ){
+            sum = sum + a.get(i);
+        }
+        return sum / a.size();
+    }
 
     public void resetMousePointer(View view) {
+
+
         // Write a message to the database
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("X");
-        myRef.setValue(0);
+        myRef.setValue(49);
         myRef = database.getReference("Y");
-        myRef.setValue(0);
+        myRef.setValue(49);
         myRef = database.getReference("Z");
-        myRef.setValue(0);
+        myRef.setValue(49);
+        doResetSensorIfThisSmallerThanTen = 0;
     }
 
     public void accept(View view){
