0780bfff57f5ff2aa8dbd133e4ba81756acbfc06
maciejlewandowski5 <maciejlewandowski5@github.com>
2020-05-14 14:14:25 +0200

    Recodeded water page to proccesing framework, added sensors listeneres and water generation

--- app/src/main/java/com/example/controler/Sketch.java
+++ app/src/main/java/com/example/controler/Sketch.java
@@ -2,17 +2,128 @@
 package com.example.controler;
 
 import processing.core.PApplet;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.hardware.SensorEventListener;
+import android.view.View;
 
-public class Sketch extends PApplet {
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
+import static java.lang.Math.asin;
+import static java.lang.Math.tan;
+
+public class Sketch extends PApplet  {
+    Context context;
+    SensorManager manager;
+    Sensor sensor;
+    AccelerometerListener listener;
+    float ax, ay, az;
+    float nx,ny,nz;
+    float alpha;
+
     public void settings() {
-        size(600, 600);
+        size(width, height);
     }
 
-    public void setup() { }
+    public void setup() {
+        context = getActivity();
+        manager = (SensorManager)context.getSystemService(Context.SENSOR_SERVICE);
+        sensor = manager.getDefaultSensor(Sensor.TYPE_GAME_ROTATION_VECTOR);
+        listener = new AccelerometerListener();
+        manager.registerListener(listener, sensor, SensorManager.SENSOR_DELAY_GAME);
+
+    }
 
     public void draw() {
+        background(255);
         if (mousePressed) {
+
+
             ellipse(mouseX, mouseY, 50, 50);
         }
+        fill(0);
+        text("X: " + ax + "\nY: " + ay + "\nZ: " + az, 0, 0, width, height);
+
+        strokeWeight(50);
+        stroke(50,50,240);
+        fill(50,50,240);
+
+        translate(width/2,height/2);
+        rotate(alpha);
+        rect(-width,0,2*width,height);
+
+        rotate(-alpha);
+        translate(-width/2,-height/2);
     }
+
+    class AccelerometerListener implements SensorEventListener {
+        private int doResetSensorIfThisSmallerThanTen = 0 ;
+        private ArrayList<Double> xi = new ArrayList<>();
+        private ArrayList<Double> yi = new ArrayList<>();
+        private ArrayList<Double> zi = new ArrayList<>();
+        private Double X;
+        private Double Y;
+        private Double Z;
+        @Override
+        public void onSensorChanged(SensorEvent sensorEvent){
+            if(doResetSensorIfThisSmallerThanTen >11) {
+               alpha = (float)(2*asin((float)(sensorEvent.values[2] - X )));
+
+
+                ax = (float)(sensorEvent.values[2] - X);
+               ay = (float)(sensorEvent.values[0] - Y);
+               az = (float)(sensorEvent.values[1] - Z);
+
+            } else if (doResetSensorIfThisSmallerThanTen <11){
+                xi.add((double) sensorEvent.values[2]);
+                yi.add((double) sensorEvent.values[0]);
+                zi.add((double) sensorEvent.values[1]);
+                doResetSensorIfThisSmallerThanTen = doResetSensorIfThisSmallerThanTen + 1;
+            }else{
+                X = getMean(xi);
+                Y = getMean(yi);
+                Z = getMean(zi);
+                xi.clear();
+                yi.clear();
+                zi.clear();
+                doResetSensorIfThisSmallerThanTen = doResetSensorIfThisSmallerThanTen + 1;
+            }
+
+
+        }
+        private Double getMean(ArrayList<Double> a){
+            Double sum = 0.0;
+            for( int i=0 ; i < a.size() ; i++ ){
+                sum = sum + a.get(i);
+            }
+            return sum / a.size();
+        }
+
+        public void resetMousePointer(View view) {
+
+
+            // Write a message to the database
+            FirebaseDatabase database = FirebaseDatabase.getInstance();
+            DatabaseReference myRef = database.getReference("X");
+            myRef.setValue(360);
+            myRef = database.getReference("Y");
+            myRef.setValue(250);
+            myRef = database.getReference("Z");
+            myRef.setValue(49);
+            doResetSensorIfThisSmallerThanTen = 0;
+        }
+
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/controler/WaterPage.java
+++ app/src/main/java/com/example/controler/WaterPage.java
@@ -5,6 +5,7 @@ import androidx.dynamicanimation.animation.DynamicAnimation;
 import androidx.dynamicanimation.animation.SpringAnimation;
 import androidx.dynamicanimation.animation.SpringForce;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.drawable.GradientDrawable;
 import android.hardware.Sensor;
@@ -22,47 +23,61 @@ import com.google.firebase.database.FirebaseDatabase;
 import static java.lang.Math.asin;
 import static java.lang.Math.tan;
 
-public class WaterPage extends AppCompatActivity implements SensorEventListener {
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 
-    private SensorManager sensorManager;
-    private Sensor sensor;
-    private SpringAnimation springAnim;
+import processing.android.PFragment;
+import processing.android.CompatUtils;
+import processing.core.PApplet;
+
+public class WaterPage extends AppCompatActivity {
+
+
+    private PApplet sketch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        LinearLayout myLayout = findViewById(R.id.WaterPage);
 
 
-        setContentView(R.layout.activity_water_page);
+        //setContentView(R.layout.activity_water_page);
 
-        final View img = findViewById(R.id.rectangle_2);
-// Setting up a spring animation to animate the view’s translationY property with the final
-// spring position at 0.
-         springAnim = new SpringAnimation(img, DynamicAnimation.ROTATION, 0);
+        FrameLayout frame = new FrameLayout(this);
+        frame.setId(CompatUtils.getUniqueViewId());
+        setContentView(frame, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT));
 
-        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_GAME_ROTATION_VECTOR);
-        sensorManager.registerListener( WaterPage.this,sensor,SensorManager.SENSOR_DELAY_GAME);
-
-
-        springAnim.setStartValue(0);
-        springAnim.setMinValue(-180);
-        springAnim.setMaxValue((float)180); // in radians
-        springAnim.getSpring().setDampingRatio(SpringForce.DAMPING_RATIO_MEDIUM_BOUNCY);
-        springAnim.getSpring().setStiffness(SpringForce.STIFFNESS_VERY_LOW);
-
+        sketch = new Sketch();
+        PFragment fragment = new PFragment(sketch);
+        fragment.setView(frame, this);
 
 
 
     }
 
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int i){
 
-    }
+
+
     @Override
-    public void onSensorChanged(SensorEvent sensorEvent){
-            springAnim.animateToFinalPosition((float)Math.toDegrees(2 * asin(sensorEvent.values[2])));
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        if (sketch != null) {
+            sketch.onRequestPermissionsResult(
+                    requestCode, permissions, grantResults);
         }
+    }
+
+    @SuppressLint("MissingSuperCall")
+    @Override
+    public void onNewIntent(Intent intent) {
+        if (sketch != null) {
+            sketch.onNewIntent(intent);
+        }
+    }
 }
+
+
