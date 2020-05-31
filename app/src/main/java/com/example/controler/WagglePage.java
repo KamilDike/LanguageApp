bb56ce399c698cc7afb155146de34f31aff56ed4
Dominika Lamch <dlamch320@gmail.com>
2020-05-31 22:22:18 +0200

    Counting trigger events - wavels in WagglePage

--- app/src/main/java/com/example/controler/WagglePage.java
+++ app/src/main/java/com/example/controler/WagglePage.java
@@ -3,7 +3,12 @@ package com.example.controler;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Service;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.hardware.TriggerEvent;
+import android.hardware.TriggerEventListener;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -17,13 +22,20 @@ import com.google.firebase.database.ValueEventListener;
 public class WagglePage extends AppCompatActivity {
     private FirebaseDatabase database;
     private DatabaseReference slideRef;
+    private DatabaseReference myRef;
+    private SensorManager sensorManager;
+    private TriggerEventListener triggerEventListener;
+    private Sensor sensor;
     private Boolean firstRead = true;
+    private int triggerCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_waggle_page);
 
+        sensorManager = (SensorManager) getSystemService(Service.SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_SIGNIFICANT_MOTION);
 
         database = FirebaseDatabase.getInstance();
         slideRef = database.getReference("slide");
@@ -62,15 +74,28 @@ public class WagglePage extends AppCompatActivity {
                 // Failed to read value
             };
         });
+        triggerEventListener = new TriggerEventListener() {
+            @Override
+            public void onTrigger(TriggerEvent event) {
+                //getSupportActionBar().setTitle("Trigger: " + event.values[0]);
+                myRef = database.getReference("trigger");
+                if(event.values[0] == 1) {
+                    triggerCount++;
+                    myRef.setValue(triggerCount);
+                    sensorManager.requestTriggerSensor(triggerEventListener, sensor);
+                }
+            }
+        };
     }
-    public void resetMousePointer(View view) {
-        // Write a message to the database
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("X");
-        myRef.setValue("0");
-        myRef = database.getReference("Y");
-        myRef.setValue("0");
-        myRef = database.getReference("Z");
-        myRef.setValue("0");
+
+    protected void onResume() {
+        super.onResume();
+        sensorManager.requestTriggerSensor(triggerEventListener, sensor);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        sensorManager.cancelTriggerSensor(triggerEventListener, sensor);
+        myRef.setValue(0);
     }
 }
