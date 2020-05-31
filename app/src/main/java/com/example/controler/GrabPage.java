854c9e7c9eff0e4fa7a2ed3aa7d1fa80a2b88277
Dominika Lamch <dlamch320@gmail.com>
2020-05-31 22:18:31 +0200

    Reading from proximity sensor in GrabPage

--- app/src/main/java/com/example/controler/GrabPage.java
+++ app/src/main/java/com/example/controler/GrabPage.java
@@ -3,10 +3,16 @@ package com.example.controler;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Service;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -17,15 +23,23 @@ import com.google.firebase.database.ValueEventListener;
 public class GrabPage extends AppCompatActivity {
     private FirebaseDatabase database;
     private DatabaseReference slideRef;
+    private DatabaseReference myRef;
+    private SensorManager sensorManager;
+    private Sensor proximitySensor;
+    private SensorEventListener proximityEventListener;
     private Boolean firstRead = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grab_page);
-        database = FirebaseDatabase.getInstance();
 
+        sensorManager = (SensorManager) getSystemService(Service.SENSOR_SERVICE);
+        proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+
+        database = FirebaseDatabase.getInstance();
         slideRef = database.getReference("slide");
+        myRef = database.getReference("proximity");
 
         slideRef.addValueEventListener(new ValueEventListener() {
             @Override
@@ -61,16 +75,36 @@ public class GrabPage extends AppCompatActivity {
                 // Failed to read value
             };
         });
+        if (proximitySensor == null) {
+            Toast.makeText(this, "No proximity sensor detected", Toast.LENGTH_SHORT).show();
+            finish();
+        }
 
+        proximityEventListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                // getSupportActionBar().setTitle("Proximity: " + event.values[0]);
+                if(event.values[0] != 0)
+                    myRef.setValue(1);
+                else
+                    myRef.setValue(0);
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            }
+        };
     }
-    public void resetMousePointer(View view) {
-        // Write a message to the database
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("X");
-        myRef.setValue("0");
-        myRef = database.getReference("Y");
-        myRef.setValue("0");
-        myRef = database.getReference("Z");
-        myRef.setValue("0");
+
+    @Override
+    protected void onResume()  {
+        super.onResume();
+        sensorManager.registerListener(proximityEventListener, proximitySensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause()  {
+        super.onPause();
+        sensorManager.unregisterListener(proximityEventListener);
     }
 }
